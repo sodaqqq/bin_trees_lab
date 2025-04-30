e77c05d7847156987a680812e6e7371ecdef2fa8
Ostap Kostiuk <164146945+sodaqqq@users.noreply.github.com>
2025-04-30 14:04:42 +0300

    Create solution.py

---
+++ delete node/solution.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        
+        elif key > root.val:
+            root.right = self.deleteNode(root.right, key)
+        
+        else:
+            if not root.left:
+                return root.right
+            elif not root.right:
+                return root.left
+            
+            min_ = self.get_min(root.right)
+            root.val = min_.val
+            root.right = self.deleteNode(root.right, min_.val)
+
+        return root
+
+    def get_min(self, node):
+            while node.left:
+                node = node.left
+            return node
+        
+        
